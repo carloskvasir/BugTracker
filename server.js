fbc28011e18c44fdd8de79de6b36be80b68ddae8
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-10 15:49:14 -0300

    create soma route

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
+)
 app.get('/', (req, res) => {
   res.send('Ok: ok')
+})
+app.get('/soma', (req, res) => {
+  const a = parseInt(req.query.a)
+  const b = parseInt(req.query.b)
+  const soma = a + b
+  
+  res.send(`<h1> soma = ${soma}.</h1>`)
 })
 
 app.listen(3000)
