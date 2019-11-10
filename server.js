1d4c251f2a3a96849504ff30ee26f5b08e8249c8
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-10 16:19:05 -0300

    implements ejs form

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
 
-
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.post('/', (req, res) => {
+  res.send('postou')
+})
 app.listen(3000)
