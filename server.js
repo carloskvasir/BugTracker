30d9855b8b1bfa478e91a48865b45a3bd15a8356
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-10 15:58:03 -0300

    using ejs

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'views'))
+
+
 app.get('/', (req, res) => {
-  res.send('Ok: ok')
+  res.render('home')
 })
 app.get('/soma', (req, res) => {
   const a = parseInt(req.query.a)
